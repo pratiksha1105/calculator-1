100779b0454f0b2d42d091654f90c3c3223897db
Pratiksha0511 <pratikshabhosale1105@gmail.com>
2022-02-01 11:14:32 +0530

    5 files modified

--- README.md
+++
@@ -1,3 +0,0 @@
-# web-platform-gfbfzl
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-gfbfzl)
--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('hello!')
+let eq = '';
+
+//function that display value
+function dis(val) {
+  eq += val;
+  document.getElementById('result').value = eq;
+}
+
+//function that evaluates the digit and return result
+function solve() {
+  eq = eval(eq);
+  document.getElementById('result').value = eq;
+}
+
+//function that clear the display
+function clr() {
+  eq = '';
+  document.getElementById('result').value = eq;
+}
+
+function initilize() {
+  console.log('Initializing');
+  document.getElementById('btn_clear').addEventListener('click', clr);
+  document.getElementById('btn_solve').addEventListener('click', solve);
+
+  const opButtons = document.getElementsByClassName('op_button');
+  for (let i = 0; i < opButtons.length; i++) {
+    opButtons[i].addEventListener('click', (event) => {
+      dis(event.target.value.toString());
+    });
+  }
+}
+
+// initilize
+initilize();
